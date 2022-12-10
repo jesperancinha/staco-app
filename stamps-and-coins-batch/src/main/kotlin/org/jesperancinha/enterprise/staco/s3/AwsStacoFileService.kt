f362d5c5195bfee577f3b51505f2059bb2d8e8b4
Joao Esperancinha <jofisaes@gmail.com>
2022-12-10 11:09:00 +0100

    Using async

--- stamps-and-coins-batch/src/main/kotlin/org/jesperancinha/enterprise/staco/s3/AwsStacoFileService.kt
+++ stamps-and-coins-batch/src/main/kotlin/org/jesperancinha/enterprise/staco/s3/AwsStacoFileService.kt
@@ -23,6 +23,7 @@ import java.nio.file.Files
 import java.nio.file.Files.newBufferedWriter
 import java.nio.file.Path
 import java.time.Instant
+import java.util.concurrent.Executors
 import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
 
@@ -64,16 +65,20 @@ class AwsStacoFileService(
         val fileIn: InputStream = Files.newInputStream(output)
         val size: Long = Files.size(output)
 
-        s3AsyncClient.putObject(
-            PutObjectRequest.builder().bucket(STACOS_BUCKET).key(fileName)
-                .metadata(
-                    mapOf(
-                        "Content-Type" to "application/x-gzip",
-                        "Content-Length" to size.toString()
-                    )
-                ).build(),
-            AsyncRequestBody.fromBytes(fileIn.readBytes())
-        )
+        Executors.newCachedThreadPool().submit {
+            s3AsyncClient.putObject(
+                PutObjectRequest.builder().bucket(STACOS_BUCKET).key(fileName)
+                    .metadata(
+                        mapOf(
+                            "Content-Type" to "application/x-gzip",
+                            "Content-Length" to size.toString()
+                        )
+                    ).build(),
+                AsyncRequestBody.fromBytes(fileIn.readBytes())
+            ).thenApplyAsync {
+                logger.info { "File $output is uploaded!" }
+            }
+        }
         s3AsyncClient.logAllBuckets()
         logger.info { "File $path was created!" }
         logger.info { "File $output is being uploaded!" }
@@ -83,43 +88,45 @@ class AwsStacoFileService(
 
     fun downloadFileFromS3UpdateDynamoDBAndDelete() {
         logger.info { "Downloading files from S3" }
-        s3AsyncClient.listObjects(
-            ListObjectsRequest.builder().bucket(STACOS_BUCKET).build()
-        ).thenApplyAsync { listObjectResponse ->
-            s3AsyncClient.logAllBuckets()
-            logger.info { "Found ${listObjectResponse.contents().size} files!" }
-            listObjectResponse.contents().map { s3Object ->
-                logger.info { "Processing object file ${s3Object.key()}" }
-                val targetFileKey = s3Object.key()
-                val path = Files.createTempFile(targetFileKey, ".gz")
-                s3AsyncClient.getObject(
-                    GetObjectRequest.builder().bucket(STACOS_BUCKET).key(targetFileKey).build(),
-                    AsyncResponseTransformer.toBytes()
-                )
-                    .thenApplyAsync {
-                        try {
-                            Files.write(path, it.asByteArray())
-                            logger.info { "Downloaded file $path" }
-                            val output = Files.createTempFile(targetFileKey, ".csv")
-                            decompressGzip(path, output)
-                            val reader = Files.newBufferedReader(output)
-                            val csvParser = CSVParser(reader, CSV_HEADER)
-                            val records = csvParser.records
-                            for (csvRecord in records.takeLast(records.size - 1)) {
-                                try {
-                                    staCoDynamoDBRepository.save(csvRecord.toEvent)
-                                } catch (ex: IllegalArgumentException) {
-                                    logger.info { "Record $csvRecord was rejected!. Reason: $ex" }
+        Executors.newCachedThreadPool().submit {
+            s3AsyncClient.listObjects(
+                ListObjectsRequest.builder().bucket(STACOS_BUCKET).build()
+            ).thenApplyAsync { listObjectResponse ->
+                s3AsyncClient.logAllBuckets()
+                logger.info { "Found ${listObjectResponse.contents().size} files!" }
+                listObjectResponse.contents().map { s3Object ->
+                    logger.info { "Processing object file ${s3Object.key()}" }
+                    val targetFileKey = s3Object.key()
+                    val path = Files.createTempFile(targetFileKey, ".gz")
+                    s3AsyncClient.getObject(
+                        GetObjectRequest.builder().bucket(STACOS_BUCKET).key(targetFileKey).build(),
+                        AsyncResponseTransformer.toBytes()
+                    )
+                        .thenApplyAsync {
+                            try {
+                                Files.write(path, it.asByteArray())
+                                logger.info { "Downloaded file $path" }
+                                val output = Files.createTempFile(targetFileKey, ".csv")
+                                decompressGzip(path, output)
+                                val reader = Files.newBufferedReader(output)
+                                val csvParser = CSVParser(reader, CSV_HEADER)
+                                val records = csvParser.records
+                                for (csvRecord in records.takeLast(records.size - 1)) {
+                                    try {
+                                        staCoDynamoDBRepository.save(csvRecord.toEvent)
+                                    } catch (ex: IllegalArgumentException) {
+                                        logger.info { "Record $csvRecord was rejected!. Reason: $ex" }
+                                    }
                                 }
+                                logger.info { "Download and parsing of file $output complete!" }
+                                removeResource(targetFileKey)
+                            } catch (ex: Exception) {
+                                removeResource(targetFileKey)
+                                logger.info { "File with Key $targetFileKey is invalid and has been removed!" }
+                                logger.error { ex }
                             }
-                            logger.info { "Download and parsing of file $output complete!" }
-                            removeResource(targetFileKey)
-                        } catch (ex: Exception) {
-                            removeResource(targetFileKey)
-                            logger.info { "File with Key $targetFileKey is invalid and has been removed!" }
-                            logger.error { ex }
                         }
-                    }
+                }
             }
         }
     }
